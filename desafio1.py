e0dc6442fbb0560b14af4677fe48d5d7655850ed
Ericha Barbosa <erichataina02@gmail.com>
2025-10-04 11:22:59 -0300

    Update desafio1.py

--- desafio1.py
+++ desafio1.py
@@ -1,7 +1,11 @@
-# n = int(input('Digite uma nota, entre zero e dez: '))
-# while n > 10 or n < 0:
-#     n = int(input("Errou, tente dnv: "))
-# print("boa")
+#Desáfio 1
+
+n = int(input('Digite uma nota, entre zero e dez: '))
+ while n > 10 or n < 0:
+    n = int(input("Errou, tente dnv: "))
+ print("boa")
+
+#Desáfio 2
 
 user = input("Digite o usuario: ")
 senha = input("Digite a senha: ")
@@ -9,4 +13,5 @@ senha = input("Digite a senha: ")
 for senha in user:
     print("Tente novamente")
     user = input("Digite o usuario: ")
+
     senha = input("Digite a senha: ")
